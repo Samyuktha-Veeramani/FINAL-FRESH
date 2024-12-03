a311673cc54b272efcedd3f5fece01acf334bee1
DANNY282005 <danielsolomon282005@gmail.com>
2024-12-03 12:09:48 +0530

    changes

--- app.py
+++ app.py
@@ -333,4 +333,4 @@ def delete_product(product_id):
     return redirect(url_for('admin_product_management'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000,debug=True)
